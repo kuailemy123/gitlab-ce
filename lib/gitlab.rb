c7cc00104e77b65c6abce4b2c3144fa7a52950a4
Robert Speicher <rspeicher@gmail.com>
2016-06-13 17:43:29 -0400

    Update `Gitlab.com?` to support staging

--- lib/gitlab.rb
+++ lib/gitlab.rb
@@ -2,6 +2,7 @@ require_dependency 'gitlab/git'
 
 module Gitlab
   def self.com?
-    Gitlab.config.gitlab.url == 'https://gitlab.com'
+    Gitlab.config.gitlab.url == 'https://gitlab.com' ||
+      Gitlab.config.gitlab.url == 'https://staging.gitlab.com'
   end
 end
