e5deb19da2b2eaedfcf268b3b53fcba01b114b2e
Douwe Maan <douwe@selenight.nl>
2016-06-20 19:22:08 +0200

    Support new diff notes on MRs with diff_refs

--- app/controllers/projects/merge_requests_controller.rb
+++ app/controllers/projects/merge_requests_controller.rb
@@ -82,6 +82,7 @@ class Projects::MergeRequestsController < Projects::ApplicationController
       noteable_id: @merge_request.id
     }
 
+    @use_legacy_diff_notes = !@merge_request.support_new_diff_notes?
     @grouped_diff_notes = @merge_request.notes.grouped_diff_notes
 
     respond_to do |format|
