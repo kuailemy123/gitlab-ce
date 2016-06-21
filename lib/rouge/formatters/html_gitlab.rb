62e9701d5151e67a8051783cb676cacfd37cc173
http://jneen.net/ <jneen@jneen.net>
2016-06-21 10:53:47 -0700

    support multiple uses of the same formatter

--- lib/rouge/formatters/html_gitlab.rb
+++ lib/rouge/formatters/html_gitlab.rb
@@ -8,15 +8,16 @@ module Rouge
       # [+linenostart+]     The line number for the first line (default: 1).
       def initialize(linenostart: 1)
         @linenostart = linenostart
+        @line_number = linenostart
       end
 
       def stream(tokens, &b)
-        token_lines(tokens).each_with_index do |line, index|
-          line_number = @linenostart + index
-
-          yield %<<span id="LC#{line_number}" class="line">>
+        token_lines(tokens) do |line|
+          yield %<<span id="LC#{@line_number}" class="line">>
           line.each { |token, value| yield span(token, value) }
           yield %<</span>\n>
+
+          @line_number += 1
         end
       end
     end
